d5c93bb25bc5601e186cc0ae2acce9694c508859
Nevendra <nevendra@live>
2016-11-04 17:00:21 -0400

    Updating gh-pages from master branch via GitBot

---
+++ collectionAngularImageGallery.js
@@ -0,0 +1,49 @@
+(function() {
+  'use strict';
+
+angular.module('CollectionImageGallery', ['DesignerService', 'ui.router'])
+
+.controller("CollectionImageGalleryController", function(DesignerListFactory, $location, $anchorScroll) {
+
+	var self = this;
+	self.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+   	}
+	self.imageToBeViewed = false;
+	self.designerList = new DesignerListFactory();
+	self.designerImages;
+	self.mainHtml = true;
+	self.selectedImage;
+	self.clickedThumbNail = false
+	self.viewGallery = function(designer) {
+		self.designerImages = self.designerList.viewDesignerGallery(designer);
+		console.log(self.designerImages);
+		self.imageToBeViewed = true;
+		self.mainHtml = false;
+
+	};
+	self.closeDesignerImageGallery = function() {
+		self.mainHtml = true;
+		self.imageToBeViewed = false;
+		self.clickedThumbNail = false
+	};
+	self.viewLargeImage = function(image) {
+		self.selectedImage = image;
+		self.imageToBeViewed = false;
+		self.clickedThumbNail = true;
+	}
+	self.closeDesignerImageGallerySelectedImage = function() {
+		self.clickedThumbNail = false;
+		self.imageToBeViewed = true;
+	}
+})
+
+.config(routerConfig)
+
+function routerConfig($stateProvider, $urlRouterProvider){
+
+}
+
+
+})();
---
+++ service.js
@@ -0,0 +1,321 @@
+(function() {
+  'use strict';
+
+angular.module('DesignerService', [])
+
+.factory('DesignerListFactory', function(){
+	function DesignerListFactory(){
+		this.designerArray = [
+			{
+				name: "Eddy K",
+				description: "Designed in Milan, the fashion capital of the world, and manufactured with the highest quality fabrics. Sophisticated couture gowns and elegant destination dresses are made with the highest attention to detail that flatters every figure, taste, and occasion.",
+				mainImage: "Images/BETTE.jpg",
+				gallery: [
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},					
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},					
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},
+					{
+						img: "Images/BETTE.jpg",
+						designerName: "Eddie k",
+					},
+				]
+
+			},
+			{
+				name: "Justin Alexander",
+				description: "One of America's and Europe's most influential producers of wedding fashions since 1949, blending contemporary style with classic details. Gowns that create a timeless romantic look with lavish lace, memorable details, and stunning silhouettes. If you love Grace Kelly, Audrey Hepburn, or Jackie Kennedy, these designs are perfect for you.",
+				mainImage: "Images/JustinAlex3.jpeg",
+				gallery: [
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},					
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},					
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},
+					{
+						img: "Images/JustinAlex3.jpeg",
+						designerName: "Justin Alexander",
+					},
+				]
+
+
+			},
+			{
+				name: "Venus",
+				description: "Timeless and romantic details come together to create a vast array of styles for every bride's individual beauty and unique personal charm. These gowns are in demand all around the world.",
+				mainImage: "Images/MaggieSotteroS5301.jpg",
+				gallery: [
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},					
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},					
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},
+					{
+						img: "Images/MaggieSotteroS5301.jpg",
+						designerName: "Venus",
+					},
+				]
+
+
+			},
+			{
+				name: "Jude Jowilson",
+				description: "Sophisticated, classic style, with a modern approach and exquisite attention to detail. Couture gowns hand-crafted in the USA, using the finest fabrics, delicate embroideries, intricate draping and dramatic silhouettes.",
+				mainImage: "Images/FLORENCE_edited.jpg",
+				gallery: [
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},					
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},					
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},
+					{
+						img: "Images/FLORENCE_edited.jpg",
+						designerName: "Jude Jowilson",
+					},
+				]
+
+
+			},
+			{
+				name: "Rosa Clará",
+				description: "Starting out in Barcelona and opening one of the world's most prestigious bridalwear shows - Barecelona Bridal Week, this brand soon spread throughout Spain and the rest of the world. Today it still offers innovative designs created from the highest quality fabrics.",
+				mainImage: "Images/GRACE_edited.jpg",
+				gallery: [
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},					
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},					
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},
+					{
+						img: "Images/GRACE_edited.jpg",
+						designerName: "Rosa Clará",
+					},
+				]
+
+
+			},
+			{
+				name: "Nicole Spose",
+				description: "Nicole Spose has collections that speak of a fresh and young fashion, that dictate trends, while maintaining quality and tailoring. Exclusively at our salon in New York City",
+				mainImage: "Images/ELIZABETH_edited.jpg",
+				gallery: [
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},					
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},					
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},
+					{
+						img: "Images/ELIZABETH_edited.jpg",
+						designerName: "Nicole Spose",
+					},
+				]
+
+
+			}
+		]/// ends designer array
+	};/// ends designerlistFactory function()
+
+	DesignerListFactory.prototype.viewDesignerGallery = function(designer) {
+		for(var i = 0; i < this.designerArray.length; i++ ){
+			this.name = this.designerArray[i];
+			if(this.name.name === designer){
+				this.designGalleryArray = [];
+				this.nameGallery = this.name.gallery;
+				this.designGalleryArray = this.nameGallery;
+			}
+		}
+		return this.designGalleryArray;
+	};
+
+	return DesignerListFactory;
+})//// ends designerlistfactory
+
+})();
+
